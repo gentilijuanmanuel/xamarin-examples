19d0cbb08fb75e40e5ab8e606a7ee89651e6fe93
Juan Manuel Gentili <juan.gentili@d-genix.com>
2018-07-24 17:56:33 -0300

    CustomTableProject added

---
+++ ExampleCustomTable/ExampleCustomTable/ItemsAdapter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+
+namespace ExampleCustomTable
+{
+    public class ItemsAdapter : BaseAdapter
+    {
+        Context context;
+
+        public ItemsAdapter(Context c)
+        {
+            context = c;
+
+            for (int i = 0; i < 200; i++)
+                this.thumbIds.Add(Resource.Drawable.sample_2);
+        }
+
+        public override int Count
+        {
+            get { return thumbIds.Count; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+
+            if (convertView == null)
+            {
+                imageView = new ImageView(context);
+                imageView.LayoutParameters = new GridView.LayoutParams(200, 200);
+                imageView.SetScaleType(ImageView.ScaleType.FitXy);
+            }
+            else
+            {
+                imageView = (ImageView)convertView;
+            }
+
+            imageView.SetImageResource(thumbIds[position]);
+            return imageView;
+        }
+
+        List<int> thumbIds = new List<int>();
+    }
+}
---
+++ ExampleCustomTable/ExampleCustomTable/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Util;
+using Android.Views;
+using System;
+using Android.Support.V7.App;
+
+namespace ExampleCustomTable
+{
+    [Activity(Label = "ExampleCustomTable", MainLauncher = true, Theme = "@style/AppTheme.Base")]
+    public class MainActivity : AppCompatActivity
+    {
+        private ScrollView scrollViewLeftDown, scrollViewRightDown;
+        private HorizontalScrollView horizontalScrollViewRightTop, horizontalScrollViewRightBottom;
+        private GridView gvRightDown;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            //scrolling
+            horizontalScrollViewRightTop = this.FindViewById<HorizontalScrollView>(Resource.Id.hs_right_top);
+            horizontalScrollViewRightBottom = this.FindViewById<HorizontalScrollView>(Resource.Id.hv_right_down);
+            scrollViewLeftDown = this.FindViewById<ScrollView>(Resource.Id.sv_left_down);
+            //scrollViewRightDown = this.FindViewById<ScrollView>(Resource.Id.sv_right_down);
+            gvRightDown = this.FindViewById<GridView>(Resource.Id.gv_example);
+            gvRightDown.Adapter = new ItemsAdapter(this);
+
+            horizontalScrollViewRightTop.ScrollChange += HorizontalScrollViewRightTop_ScrollChange;
+            horizontalScrollViewRightBottom.ScrollChange += HorizontalScrollViewRightTop_ScrollChange;
+
+            scrollViewLeftDown.ScrollChange += ScrollViewLeftDown_ScrollChange;
+            gvRightDown.ScrollChange += ScrollViewLeftDown_ScrollChange;
+            //scrollViewRightDown.ScrollChange += ScrollViewLeftDown_ScrollChange;
+        }
+
+        void ScrollViewLeftDown_ScrollChange(object sender, View.ScrollChangeEventArgs e)
+        {
+            if (((View)sender).Id == Resource.Id.sv_left_down)
+            {
+                gvRightDown.ScrollTo(0, e.ScrollY);
+                //scrollViewRightDown.ScrollTo(0, e.ScrollY);
+            }
+            else
+            {
+                Console.WriteLine(e.ScrollY);
+                scrollViewLeftDown.ScrollTo(0, e.ScrollY);
+            }
+        }
+
+        void HorizontalScrollViewRightTop_ScrollChange(object sender, View.ScrollChangeEventArgs e)
+        {
+            if (((View)sender).Id == Resource.Id.hs_right_top)
+            {
+                horizontalScrollViewRightBottom.ScrollTo(e.ScrollX, 0);
+            }
+            else
+            {
+                horizontalScrollViewRightTop.ScrollTo(e.ScrollX, 0);
+            }
+        }
+    }
+}
